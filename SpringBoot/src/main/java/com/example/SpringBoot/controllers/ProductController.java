9a5e108ea92492c2f3cfdd9cd0d57329a0c7fb6e
Gustavo <gustavosales0704@gmail.com>
2024-11-20 01:05:33 -0300

    Controller: implementando metodos PUT e DELETE

--- SpringBoot/src/main/java/com/example/SpringBoot/controllers/ProductController.java
+++ SpringBoot/src/main/java/com/example/SpringBoot/controllers/ProductController.java
@@ -24,6 +24,7 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository; //ponto de injeção para acesso aos metodos
 
+    //adicionar o produto
     @PostMapping("/products") //metodo de adicionar itens
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
         var productModel = new ProductModel();
@@ -31,11 +32,13 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
+    //listar todos os produtos
     @GetMapping("/products") //metodo de leitura
     public ResponseEntity<List<ProductModel>> getAllProducts(){ //no corpo possuira uma lista de produtos
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll()); //status OK (200)
     }
 
+    //listar somente um produto especifico pelo id
     @GetMapping("/products/{id}")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
         Optional<ProductModel> productO = productRepository.findById(id);
@@ -43,5 +46,30 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+
+    //atualizar o produto procurando ele pelo id
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct (@PathVariable(value= "id") UUID id,
+                                                 @RequestBody @Valid ProductRecordDto productRecordDto){
+        Optional<ProductModel> productO = productRepository.findById(id); //procura o id do produto no banco de dados
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found.");
+        }
+        var productModel = productO.get(); //traz o produto que precisa ser atualizado
+        //copia as propriedades de um parametro para outro, copyProperties(param1, param2)
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+
+    //Deletar um produto
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct (@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found.");
+        }
+        productRepository.delete(productO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
     }
 }
