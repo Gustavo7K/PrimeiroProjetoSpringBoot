a4b8075a0802ded768ae2489e2d65f8c1e10645b
Gustavo <gustavosales0704@gmail.com>
2024-11-21 23:22:38 -0300

    Finalizando o projeto inserindo HATEOAS e hipermidias

--- SpringBoot/src/main/java/com/example/SpringBoot/controllers/ProductController.java
+++ SpringBoot/src/main/java/com/example/SpringBoot/controllers/ProductController.java
@@ -14,6 +14,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 ///Um DTO (Data Transfer Object) é usado para transferir dados entre o cliente e o servidor
 ///A classe ResponseEntity em Java é uma abstração que permite manipular a resposta HTTP de um endpoint, incluindo o cabeçalho, o código de estado e o corpo
 ///A classe Optional do Java é uma forma de lidar com valores que podem ser nulos de forma mais segura e explícita
@@ -34,8 +37,18 @@ public class ProductController {
 
     //listar todos os produtos
     @GetMapping("/products") //metodo de leitura
-    public ResponseEntity<List<ProductModel>> getAllProducts(){ //no corpo possuira uma lista de produtos
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll()); //status OK (200)
+    public ResponseEntity<List<ProductModel>> getAllProducts(){//no corpo possuira uma lista de produtos
+        //inserindo hipermidias
+        List<ProductModel> productsList = productRepository.findAll();
+        if (!productsList.isEmpty()){
+            /* Para cada produto primeiro se obtem o id do produto e em seguida adiciona
+            o link que direciona a aquele produto em especifico */
+            for (ProductModel product : productsList){
+                UUID id = product.getIdProduct();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList); //status OK (200)
     }
 
     //listar somente um produto especifico pelo id
@@ -45,6 +58,9 @@ public class ProductController {
         if (productO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found.");
         }
+        //hipermidia para mostrar enquanto lista o produto o link referenciando a lista com todos os produtos
+        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products List"));
+
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
 
